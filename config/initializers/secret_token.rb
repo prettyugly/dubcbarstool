8d1df22ba7037c8aba19432fe777e8adeacd4421
Al Rivera <your.prettyuglyproductions@gmail.com>
2012-06-28 03:59:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dubcbarstool::Application.config.secret_token = '8eb331accd7220fe2faf8b95f7faccfff7f4283b8ac6cea80fef82b2b27a39d07a2e85c82b98094cbeba55f69aefa300a4e82fc4fde315927b38e5f389b39e19'
